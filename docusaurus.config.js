606a93c4af54717ff23f5ff56d5e46fd71a9d48a
Nazake <imnazake@gmail.com>
2025-01-06 09:21:44 +0100

    Update docusaurus.config.js
    github config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/imnazake/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Nazake', // Usually your GitHub org/user name.
-  projectName: 'Nazake', // Usually your repo name.
+  organizationName: 'imnazake', // Usually your GitHub org/user name.
+  projectName: 'imnazake', // Usually your repo name.
+
+  deploymentBranch: "release",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
