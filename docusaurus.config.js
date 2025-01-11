c04d384a30b7dd618df906c1017710fcb316bc26
Nazake <imnazake@gmail.com>
2025-01-11 08:58:51 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,10 +22,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'imnazake', // Usually your GitHub org/user name.
+  organizationName: 'imnazake.github.io', // Usually your GitHub org/user name.
   projectName: 'imnazake', // Usually your repo name.
+  trailingSlash: false,
 
-  deploymentBranch: "release",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
