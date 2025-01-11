03a666b60a633eb8b8a0178c4cdcfc55107e9874
Nazake <imnazake@gmail.com>
2025-01-11 09:16:10 +0100

    Updated the configs.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,25 +14,19 @@ const config = {
   tagline: 'Game Development',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
+  // Set the production URL of your site here
   url: 'https://imnazake.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/imnazake',
+  // Set the baseUrl to root ('/') for serving without subdirectories
+  baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'imnazake', // Usually your GitHub org/user name.
-  projectName: 'imnazake', // Usually your repo name.
+  // GitHub Pages deployment config
+  organizationName: 'imnazake', // Your GitHub username
+  projectName: 'imnazake', // Your repo name
   trailingSlash: false,
-
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
