eecbcd4f3e8c94da5117760b634927a53147d872
imnazake <imnazake@gmail.com>
2025-06-12 12:24:16 +0100

    Deploy website - based on bb84fcf533ad4bd84939b424815c6226c39efa9a

---
+++ assets/js/acef0c97.db6def81.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknazake=self.webpackChunknazake||[]).push([[5941],{4258:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/imnazake/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
